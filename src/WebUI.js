251a16402415e8c7a9c6f3eb3b60a6ca7f22b6d2
Saleh Jomar <salehjomar14@gmail.com>
2021-07-05 16:48:20 +0800

    Added add-task button and task display. Also integrated some UI functions.

--- src/WebUI.js
+++ src/WebUI.js
@@ -3,21 +3,34 @@ export default class WebUI {
     static overlay = document.querySelector('.overlay');
     static addProjectForm = document.querySelector('.add-project-popup');
     static addProjectButton = document.getElementById('button-add-project');
-    static cancelButton = document.querySelectorAll('.cancel');
+    static projectsButton = document.querySelectorAll('.button-default, .button-projects');
     
     static addProjectPopup(){
+        WebUI.addProjectForm.reset();
         WebUI.overlay.classList.toggle('active');
         WebUI.addProjectForm.classList.toggle('active');
     }
 
+    static displayProjectTitle(e){
+        const projectTitle = document.querySelector('#current-project-title')
+        projectTitle.innerHTML = e.target.innerHTML;
+    }
+
     static Initialize(){
+        //ADD PROJECT POPUP EVENTS
         window.addEventListener('keydown', (e) => {
             if (WebUI.overlay.classList.contains('active') && (e.key === "Escape")) WebUI.addProjectPopup();
         });
         WebUI.overlay.addEventListener('click',WebUI.addProjectPopup);
         WebUI.addProjectButton.addEventListener('click',WebUI.addProjectPopup);
-        WebUI.cancelButton.forEach(button => {
-            button.addEventListener('click', WebUI.addProjectPopup);
+        WebUI.addProjectForm.addEventListener('submit', (e)=>{
+            e.preventDefault();
+            WebUI.addProjectPopup();
+        })
+
+        //Change Project Title Event
+        WebUI.projectsButton.forEach(button => {
+            button.addEventListener('click', WebUI.displayProjectTitle);
         })
     }    
 }
